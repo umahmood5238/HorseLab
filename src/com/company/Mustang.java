c5a8574fdb8a83ab3cd935c5a57fb6179c1c1805
BT_1N3_31@bktech.local <BT_1N3_31@bktech.local>
2018-11-16 08:42:28 -0500

    Initial commit

---
+++ src/com/company/Mustang.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Mustang implements Horse
+{
+    private String name;
+    private int weight;
+    public Mustang(String name, int weight)
+    {
+        
+    }
+}
